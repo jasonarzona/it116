c296ded35e307614690382184d0789a278a5610a
jasonarzona <jarjona@sea.yearup.org>
2022-06-04 17:58:15 -0700

    comments and practice

--- domwalkthrough.js
+++ domwalkthrough.js
@@ -1,91 +1,96 @@
-console.dir(document);
+// console.dir(document);
 
-// gives the domain of webpage
-console.log(document.domain);
+// // gives the domain of webpage
+// console.log(document.domain);
 
-// gives url of the webpage
-console.log(document.URL);
+// // gives url of the webpage
+// console.log(document.URL);
 
-// the document attribute can access different elements
+// // the document attribute can access different elements
 
-console.log(document.title);
+// console.log(document.title);
 
-// if we want to know the doctype then use this code
-console.log(document.doctype);
+// // if we want to know the doctype then use this code
+// console.log(document.doctype);
 
-// shows everything in the head element
-console.log(document.head);
+// // shows everything in the head element
+// console.log(document.head);
 
-// same as the comment above
-console.log(document.body);
+// // shows everything in the body
+// console.log(document.body);
 
-// all is deprecated but still works
-console.log(document.all);
+// // all is deprecated but still works
+// console.log(document.all);
 
-// shows specific section of index
-console.log(document.all[10]);
+// // shows specific section of index
+// console.log(document.all[10]);
 
-// allows us to go in and change a particular thing through JS
-document.all[10].textContent = "I just changed the header";
+// // allows us to go in and change a particular thing through JS
+// document.all[10].textContent = "I just changed the header";
 
-//this goes through the tree to see which element is the form and displays in the console window
-console.log(document.forms);
+// //this goes through the tree to see which element is the form and displays in the console window
+// console.log(document.forms);
 
-//same as above
-console.log(document.images);
+// //same as above but images
+// console.log(document.images);
 
-//same
-console.log(document.links);
+// //same as above but links
+// console.log(document.links);
 
-/////////////////////
-GetElementByID
+// /////////////////////////////
+// //////////GetElementByID
 
-console.log(document.getElementById('header-title'));
+// //this lets us edit whatever is in the header-title id
+// console.log(document.getElementById('header-title'));
 
-//
-let headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
+// // "let" assigns a variable to headerTitle. In this case it assigned the code above that lets us edit whatever is in the header-title id
+// let headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
 
-headerTitle.textContent = "Changed the Title again"
-headerTitle.innerText = "Hello, changed you again"
-
-headerTitle.style.color = "hotpink";
+// //we changed the text in this block of code
+// headerTitle.textContent = "Changed the Title again"
+// headerTitle.innerText = "Hello, changed you again"
+// headerTitle.style.color = "hotpink";
 
 ////////////////////////////////////
 ///////////////GetElementByClassName
 
-var items = document.getElementsByClassName('list-group-item');
+// // we did the same thing as above but by class instead of id
+// var items = document.getElementsByClassName('list-group-item');
 
-console.log(items);
+// //shows everything in the items element through the console
+// console.log(items);
 
-//this makes us style the 2nd item in the array referencing them by the index
-items[1].style.fontWeight = 'bold';
-items[1].textContent = 'asdfkljghaslkf';
-items[1].style.color = 'green';
+// //this makes us style the 2nd item in the array referencing them by the index
+// items[1].style.fontWeight = 'bold';
+// items[1].textContent = 'asdfkljghaslkf';
+// items[1].style.color = 'green';
 
-items[0].style.fontWeight = 'bold';
-items[0].textContent = 'deez';
-items[0].style.color = 'brown';
+// //this makes us style the 1st item in the array referencing them by the index
+// items[0].style.fontWeight = 'bold';
+// items[0].textContent = 'deez';
+// items[0].style.color = 'brown';
 
-items[2].style.fontWeight = 'bold';
-items[2].textContent = 'nuts';
-items[2].style.color = 'red';
-items[2].style.backgroundColor = 'blue';
+// //this makes us style the 3rd item in the array referencing them by the index
+// items[2].style.fontWeight = 'bold';
+// items[2].textContent = 'nuts';
+// items[2].style.color = 'red';
+// items[2].style.backgroundColor = 'blue';
 
-//creates a for loop until a condition(i<=10) is met then stops 
-for(let i = 1; i <=10; i++ ){
-    console.log(i);
-}
+// //creates a for loop until a condition(i<=10) is met then stops 
+// for(let i = 1; i <=10; i++ ){
+//     console.log(i);
+// }
 
-//same as above but += is in increments of 2
-for (let i = 1; i <=20; i+=2){
-    console.log(i);
-}
+// //same as above but += is in increments of 2
+// for (let i = 1; i <=20; i+=2){
+//     console.log(i);
+// }
 
-//same as above but -= is in decrements of 2
-for (let i = 100; i >= 0; i-=2){
-    console.log(i);
-}
+// //same as above but -= is in decrements of 2
+// for (let i = 100; i >= 0; i-=2){
+//     console.log(i);
+// }
 
 //[this bracket creates an array btw]
 //lets us call an array????
