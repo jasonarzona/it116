a396b379b5f4af1a2a53c93ce51afbd3db783703
jasonarzona <jarjona@sea.yearup.org>
2022-06-07 15:29:35 -0700

    if conditions

---
+++ conditions.js
@@ -0,0 +1,16 @@
+console.log("Hello, Bitch");
+
+
+let randomNumber = Math.random(); //0.0-1
+
+//
+// if (randomNumber < 0.50) {
+//     console.log("Condition was met");
+//     console.log(randomNumber);
+// }
+
+//
+if (randomNumber >= 0.5){
+    console.log("Condition was met");
+    console.log(randomNumber);
+}
